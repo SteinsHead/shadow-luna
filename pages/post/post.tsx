9dc2b9e2599bf2613c4ce3c3a46022a9f3864c5d
christina <headsteins@gmail.com>
2022-08-25 00:07:32 +0800

    init

---
+++ pages/post/post.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Post() {
+    return (
+        <div className="container">
+            <main>
+                <h1>hello</h1>
+            </main>
+        </div>
+    )
+}
